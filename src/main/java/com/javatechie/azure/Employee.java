9fcc4409a74b5de887991222a576771b330389f6
TheGoodSan <emaslovenski@gmail.com>
2024-12-23 18:32:31 +0100

    Update Employee.java

--- src/main/java/com/javatechie/azure/Employee.java
+++ src/main/java/com/javatechie/azure/Employee.java
@@ -21,4 +21,28 @@ public class Employee {
     private String name;
     private String dept;
     private long salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
 }
